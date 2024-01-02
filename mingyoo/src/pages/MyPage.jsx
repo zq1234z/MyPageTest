1c605ab0eec088e6082b495ce09172dd6986ef02
zq1234z <zzpp430@gmail.com>
2024-01-03 00:21:02 +0900

    마이페이지 레이아웃 제작

---
+++ mingyoo/src/pages/MyPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Form,Title} from "../pages/MyPageStyle"
+
+export const MyPage = () => {
+  return(
+    <Form>
+      <Title> My Page</Title>
+      
+    </Form>
+  )
+}
