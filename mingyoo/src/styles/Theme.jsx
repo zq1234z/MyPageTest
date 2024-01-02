ab2a99b82a2666cde913391dc7c2fcaa9f3e293a
zq1234z <zzpp430@gmail.com>
2024-01-02 23:46:26 +0900

    add Theme

---
+++ mingyoo/src/styles/Theme.jsx
@@ -0,0 +1,19 @@
+export const colors = {
+  mainColor: "#40A6DE",
+  textColor: "#767676",
+  blackColor: "#000000",
+  whiteColor: "#ffffff"
+};
+
+export const fontSize = {
+  xsmall: "0.625rem", // 10px
+  small: "0.75rem", //12px
+  mediun: "0.875rem", // 14px
+  large: "1.125rem", // 18px
+  xlarge: "1.5rem", // 24px
+};
+
+export const Theme = {
+  colors,
+  fontSize,
+};
