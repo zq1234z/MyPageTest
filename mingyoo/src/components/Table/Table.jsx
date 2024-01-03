19d089d3afb780fbc1758d356d3ba2c441eaa40d
zq1234z <zzpp430@gmail.com>
2024-01-03 16:19:02 +0900

    테이블 초기 세팅 , 레이아웃 or 스타일

---
+++ mingyoo/src/components/Table/Table.jsx
@@ -0,0 +1,10 @@
+import TableStyle,{Table1} from './TableStyle'
+import { UserProfile } from '../UserProfile/UserProfile' 
+
+export const Table = () => {
+  return(
+    <Table1>
+      <UserProfile/>
+    </Table1>
+  )
+}
---
+++ mingyoo/src/components/Table/TableStyle.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Table1 = styled.table`
+  border: 3px solid black; // 테두리는 검은색
+  background-color: white; // 배경은 흰색
+  width: 100%; // 테이블의 너비를 설정, 필요에 따라 조정 가능
+  height: 100%; // 테이블의 높이를 설정, 필요에 따라 조정 가능
+`;
