bcaf0651ced416a5fe7708065b94345d8058948f
zq1234z <zzpp430@gmail.com>
2024-01-03 17:01:18 +0900

    테이블 유저프로필 스타일 2차수정

--- mingyoo/src/components/Table/TableStyle.jsx
+++ mingyoo/src/components/Table/TableStyle.jsx
@@ -6,6 +6,9 @@ export const Table1 = styled.table`
   width: 1000px; // 테이블의 너비를 설정, 필요에 따라 조정 가능
   height: 1000px; // 테이블의 높이를 설정, 필요에 따라 조정 가능
   box-shadow: 5px 5px 5px 5px gray;
+  display: flex;
+  justify-content: center; // 가로 방향으로 중앙 정렬
+  align-items: flex-start; // 세로 방향으로 중앙 정렬
 `;
 
 export const Td = styled.td`
@@ -13,4 +16,5 @@ export const Td = styled.td`
   width: 500px;
   height: 100%;
   box-shadow: -5px 5px 5px -2px gray;
+  margin-left: 10%;
 `;
--- mingyoo/src/components/UserProfile/UserProfileStyle.jsx
+++ mingyoo/src/components/UserProfile/UserProfileStyle.jsx
@@ -7,4 +7,5 @@ export const Profile = styled.div`
   object-fit: cover;
   background-color: white;
   box-shadow: 1px 3px 5px 5px gray;
+  margin-top: 10px;
 `;
